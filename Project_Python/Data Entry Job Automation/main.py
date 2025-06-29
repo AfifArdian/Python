1ddda47bfef7cbffaebd3c7a4f4c33d95669bb3d
Afif Ardian <gracejyuviolet@gmail.com>
2025-06-29 20:33:46 +0700

    Update

--- Project_Python/Data Entry Job Automation/main.py
+++ Project_Python/Data Entry Job Automation/main.py
@@ -4,26 +4,41 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
+# Part 1 - Scrape the links, addresses, and prices of the rental properties
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf4QG7WBkw0OtOXybykGMYk3IOwIK0LrSVhRrYvvUiNIYmz5w/viewform?usp=header"
-ZILLOW_URL = "https://appbrewery.github.io/Zillow-Clone/"
 
-response = requests.get(url=ZILLOW_URL)
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+}
+
+response = requests.get(url="https://appbrewery.github.io/Zillow-Clone/", headers=header)
 response.raise_for_status()
 website_html = response.text
 
 soup = BeautifulSoup(website_html, "html.parser")
 # print(soup.prettify())
 
+# Create a list of all the links
 all_link_property = [link.get('href') for link in soup.find_all(name="a", class_="property-card-link")]
-# print(all_link_property)
+print(f"There are {len(all_link_property)} links to individual listings in total: \n")
+print(all_link_property)
 
+# Create a list of all the prices
+# Get a clean dollar price and strip off any "+" symbols and "per month" /mo abbreviation
 all_price_property = [price.getText().strip('+ 1bd +/mo') for price in soup.find_all(name="span", class_="PropertyCardWrapper__StyledPriceLine")]
-# print(all_price_property)
+print(f"\n After having been cleaned up, the {len(all_price_property)} prices now look like this: \n")
+print(all_price_property)
 
+# Create a list of all the addresses
+# Remove newlines \n, pipe symbols |, and whitespaces to clean up the address data
 all_address_property = [address.getText().replace('|', '').strip() for address in soup.find_all(name="address")]
-# print(all_address_property)
+print(f"\n After having been cleaned up, the {len(all_address_property)} addresses now look like this: \n")
+print(all_address_property)
 
+# Part 2 - Fill in the Google Form using Selenium
 
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 drive = webdriver.Chrome(options=chrome_options)
