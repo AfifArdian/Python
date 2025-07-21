80ddb076f66522f111a0b527a995a0e8b3ab1067
Afif Ardian <gracejyuviolet@gmail.com>
2025-07-21 10:37:59 +0700

    Update

--- Learning_Python/Deep-Learning/Algoritma_Backprogagation.py
+++ Learning_Python/Deep-Learning/Algoritma_Backprogagation.py
@@ -14,6 +14,7 @@ x = [
     [0, 1],
     [0, 0]
 ]
+
 t = [0, 1, 1, 0] # neuron target t, studi kasus: AND, OR, AND NOT, XOR
 b = 1
 
@@ -22,11 +23,11 @@ v = [[random.uniform(-1, 1) for _ in range(3)] for _ in range(3)] # kolom pertam
 w = [random.uniform(-1, 1) for _ in range(4)]
 
 # Membuat array/list untuk menyimpan nilai delta_v, delta_w, teta_in, z, dan teta_hidden
-dv = [[0] * 3 for _ in range(3)]
-dw = [0] * 4
+delta_v: list[list[float]] = [[0] * 3 for _ in range(3)]
+delta_w = [0] * 4
 z = [0] * 3
 teta_in = [0] * 3
-teta_h = [0] * 3
+teta_hidden = [0] * 3
 
 print("\n----------------------------------------------------")
 print("Bobot Awal/inisialisasi")
@@ -41,13 +42,13 @@ print("----------------------------------------------------")
 
 # Langkah 1: Jika kondisi penghentian belum terpenuhi, lakukan langkah 2 sampai dengan 8
 alpha = 0.8
-max_iter = 1000
+max_iter = 10000
 threshold_mse = 0.01
-k = 0
-ulang = True
+epoch = 0
+loop = True
 
 # Training
-while k < max_iter and ulang:
+while epoch < max_iter and loop:
     t_error = 0
     # Langkah 2: Untuk setiap pasang data pelatihan, lakukan langkah 3 sampai dengan 8
     for d in range(4):
@@ -77,43 +78,43 @@ while k < max_iter and ulang:
         teta = error * y * (1 - y)
 
         # Hitung perubahan bobot wkj (dengan alpha=0.2):
-        dw[0] = alpha * teta * b
+        delta_w[0] = alpha * teta * b
         for j in range(3):
-            dw[j + 1] = alpha * teta * z[j]
+            delta_w[j + 1] = alpha * teta * z[j]
 
         # Langkah 7: Hitung penjumlahan kesalahan dari unit tersembunyi (=teta)
         for j in range(3):
             teta_in[j] = teta * w[j + 1]
 
             # Faktor kesalahan d di unit tersembunyi:
-            teta_h[j] = teta_in[j] * z[j] * (1 - z[j])
+            teta_hidden[j] = teta_in[j] * z[j] * (1 - z[j])
 
             # Hitung perubahan bobot ke unit tersembunyi (dengan alpha=0.2):
-            dv[j][0] = alpha * teta_h[j] * b
+            delta_v[j][0] = alpha * teta_hidden[j] * b
             for i in range(2):
-                dv[j][i + 1] = alpha * teta_h[j] * x[d][i]
+                delta_v[j][i + 1] = alpha * teta_hidden[j] * x[d][i]
 
         # Langkah 8: Hitung semua perubahan bobot
 
         # Perubahan bobot unit keluaran:
         for j in range(4):
-            w[j] += dw[j]
+            w[j] += delta_w[j]
 
         # Perubahan bobot unit tersembunyi:
         for j in range(3):
             for i in range(3):
-                v[j][i] += dv[j][i]
+                v[j][i] += delta_v[j][i]
 
     # ******************************
     # hitung Mean Square Errod (MSE)
     mse = t_error / 4
     if mse < threshold_mse:
-        ulang = False
+        loop = False
     # ******************************
-    k += 1
+    epoch += 1
 
 print("\n----------------------------------------------------")
-print(f"Bobot setelah proses training sebanyak {k} iterasi")
+print(f"Bobot setelah proses training sebanyak {epoch} iterasi")
 print("----------------------------------------------------")
 for i in range(3):
     for j in range(3):
@@ -129,7 +130,7 @@ print("----------------------------------------------------")
 print("\n\n---------------")
 print("Proses Testing")
 print("---------------")
-print("x1  x2   b   y")
+print(" x1  x2   b   y")
 print("---------------")
 for d in range(4):
     for j in range(3):
@@ -145,5 +146,5 @@ for d in range(4):
     # y = 1 / (1 + math.exp(-y_in))
     y = 1 / (1 + pow(EKSPONESIAL, -y_in))
 
-    print(f"{x[d][0]:2.0f} {x[d][1]:2.0f}   {b:2.0f}   {round(y):.0f}")
+    print(f"{x[d][0]:2.0f}  {x[d][1]:2.0f}   {b:2.0f}   {round(y):.0f}")
 print("---------------")
